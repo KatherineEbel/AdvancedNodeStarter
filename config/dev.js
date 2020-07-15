a3af5b239a100434319107b8d48080792d3a9829
Katherine Ebel <kathyebel@me.com>
2020-07-15 09:54:24 -0400

    Add caching with redis for user blog posts

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://kat201:password1@ds061777.mlab.com:61777/blog_dev',
   cookieKey: '123123123',
 };
---
+++ services/cache.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const redis = require('redis')
+const util = require('util')
+
+const url = `redis://127.0.0.1:6379`
+const client = redis.createClient(url)
+client.hget = util.promisify(client.hget)
+const exec = mongoose.Query.prototype.exec
+
+mongoose.Query.prototype.cache = function (options = {}) {
+  this.cache = true
+  this.hashKey = options.key && JSON.stringify(options.key)
+  return this
+}
+
+mongoose.Query.prototype.exec = async function () {
+  if (!this.cache || !this.hashKey) {
+    return exec.apply(this, arguments)
+  }
+  console.log(this.hashKey)
+  const key = JSON.stringify(Object.assign({}, this.getQuery(),
+    {
+      collection: this.mongooseCollection.name
+    }))
+  const cacheVal = await client.hget(this.hashKey, key)
+  if (cacheVal) {
+    const data = JSON.parse(cacheVal)
+    return Array.isArray(data)
+      ? data.map(doc => new this.model(doc))
+      : new this.model(data)
+  }
+  const result = await exec.apply(this, arguments)
+  client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10)
+  return result
+}
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey))
+  }
+}
